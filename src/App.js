b18358124ea36b71f916955755856a0209a6cd75
ramrami-B <wislely66@gmail.com>
2024-03-23 16:55:07 +0900

    style: prettier, eslint, husky setting

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import AppRoutes from './routes';
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import AppRoutes from "./routes";
 
 const App = () => {
   return (
