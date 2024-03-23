dc5fc977109a6e26a0c029b98057e253456295ef
ramrami-B <wislely66@gmail.com>
2024-03-23 16:55:07 +0900

    style: prettier, eslint, husky setting

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,9 +1,10 @@
 module.exports = {
   printWidth: 100, // printWidth default 80 => 100 으로 변경
-  singleQuote: true, // "" => ''
+  singleQuote: false, // "" => ''
   semi: true,
-  arrowParens: 'avoid', // arrow function parameter가 하나일 경우 괄호 생략
+  arrowParens: "avoid", // arrow function parameter가 하나일 경우 괄호 생략
   tabWidth: 2,
   bracketSpacing: true,
   bracketSameLine: false,
+  quoteProps: "consistent",
 };
