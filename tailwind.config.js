3fe62960475eb9cee24b2f500f28a8b2c0b8b2d1
ramrami-B <hrb.harper@gmail.com>
2024-04-08 11:36:39 +0900

    Design: tailwind background color 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@ module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
     extend: {},
+    backgroundColor: {
+      "light-green": "#DBEEDB",
+      "green": "#4CAB4A",
+      "dark-green": "#2A4F27",
+      "cheek": "#FF9595",
+    },
     colors: {
       "light-green": "#DBEEDB",
       "green": "#4CAB4A",
