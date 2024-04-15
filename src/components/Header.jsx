4763e264b27cd34cdeb770cc4f0411b8a6f94b47
ramrami-B <hrb.harper@gmail.com>
2024-04-15 22:02:35 +0900

    Feat: UserInfo 컴포넌트 클릭시 마이페이지, 로그아웃 나오기

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,12 +6,12 @@ export const Header = () => {
   return (
     <header className="fixed top-0 left-0 w-full h-[8vh] flex justify-between items-center p-8 bg-white">
       <div className="flex gap-6 items-baseline">
-        <h1 className="text-3xl text-dark-green font-nps-bold cursor-pointer hover:animate-pulse">
+        <h1 className="text-3xl text-dark-green font-nps-bold cursor-pointer hover:animate-bounce">
           쑥쑥쑥
         </h1>
         {menus.map((menu, idx) => {
           return (
-            <nav key={idx} className="hover:text-dark-green hover:font-nps-bold cursor-pointer">
+            <nav key={idx} className="hover:text-dark-green cursor-pointer">
               {menu}
             </nav>
           );
