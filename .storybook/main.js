1774469552269e32c91862c5de36a60926030b57
ramrami-B <hrb.harper@gmail.com>
2024-04-17 10:45:53 +0900

    Fix: storybook에서 svg 사용 되도록 설정

--- .storybook/main.js
+++ .storybook/main.js
@@ -17,5 +17,17 @@ const config = {
   docs: {
     autodocs: "tag",
   },
+  webpackFinal: config => {
+    const fileLoaderRule = config.module.rules.find(rule => rule.test && rule.test.test(".svg"));
+    fileLoaderRule.exclude = /\.svg$/;
+
+    config.module.rules.push({
+      test: /\.svg$/,
+      enforce: "pre",
+      loader: require.resolve("@svgr/webpack"),
+    });
+
+    return config;
+  },
 };
 export default config;
