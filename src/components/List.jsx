88d98a3cf8b2b3da361bf919cc5f0a4de2f03343
ramrami <developer.venice@gmail.com>
2024-04-19 10:33:40 +0900

    Feat: List 컴포넌트 생성

---
+++ src/components/List.jsx
@@ -0,0 +1,57 @@
+import PropTypes from "prop-types";
+import EmptyStar from "../assets/empty_star.svg";
+import FullStar from "../assets/full_star.svg";
+import { useState } from "react";
+
+const page = window.location.pathname.split("/")[1];
+
+export const List = ({ title, participantsCount, isBookmarked }) => {
+  const [isClickBookmarked, setIsClickBookmarked] = useState(isBookmarked);
+
+  const handlerClickStar = () => {
+    setIsClickBookmarked(!isClickBookmarked);
+  };
+
+  const handlerClickList = () => {};
+
+  const handlerClickTrash = () => {};
+
+  return (
+    <div
+      className="flex justify-between items-center w-full h-[8vh] p-5 my-3 bg-white rounded-lg border-[0.5px] border-gray shadow hover:cursor-pointer hover:shadow-sm"
+      onClick={handlerClickList}
+    >
+      <div className="flex gap-5 items-center">
+        {page === "" ? (
+          <div onClick={handlerClickStar}>
+            <svg width="35" height="35" viewBox="0 0 40 40">
+              {isClickBookmarked ? <FullStar /> : <EmptyStar />}
+            </svg>
+          </div>
+        ) : (
+          <span className="m-[-0.5rem]"></span>
+        )}
+        <p className="nps-bold text-lg">{title}</p>
+      </div>
+      {page === "" ? (
+        <span
+          className="material-symbols-outlined text-2xl hover:text-3xl"
+          onClick={handlerClickTrash}
+        >
+          delete
+        </span>
+      ) : (
+        <div className="flex items-center gap-1">
+          <span className="material-symbols-outlined text-2xl">groups</span>
+          <span className="text-lg">{participantsCount}</span>
+        </div>
+      )}
+    </div>
+  );
+};
+
+List.propTypes = {
+  title: PropTypes.string,
+  participantsCount: PropTypes.number,
+  isBookmarked: PropTypes.bool,
+};
