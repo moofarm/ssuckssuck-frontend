1415ab1d32b831db6492a8058e1025b527a9b566
ramrami-B <hrb.harper@gmail.com>
2024-04-21 19:23:30 +0900

    Feat: 페이지 라우팅 구축

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,15 @@
+import { Link } from "react-router-dom";
+
 const HomePage = () => {
   return (
-    <div>
+    <div className="page">
       <h1 className="text-3xl font-bold underline">HomePage</h1>
+      <Link to="login">로그인 페이지로 이동</Link>
+      <Link to="signup">회원가입 페이지로 이동</Link>
+      <Link to="mymissions">나의미션방 페이지로 이동</Link>
+      <Link to="missions">전체미션방 페이지로 이동</Link>
+      <Link to="group">미션방 내부 페이지로 이동</Link>
+      <Link to="mypage">마이페이지로 이동</Link>
     </div>
   );
 };
