32dc91fae1d45ec232361982980acfde74e775d9
고승희 <bookshelf1234@ewhain.net>
2024-05-03 02:22:12 +0900

    Design: Tab 피드백대로 디자인 수정(폰트 크기, 화살표 함수)

--- src/components/Tab.jsx
+++ src/components/Tab.jsx
@@ -1,66 +1,53 @@
-// Tab.jsx
+import React, { useState } from "react";
+import { categories } from "../utils/utils.js";
+const Tab = () => {
+  const [selectedCategory, setSelectedCategory] = useState("공부");
+  const [selectedSubcategory, setSelectedSubcategory] = useState(categories["공부"][0]);
 
-import React, { Component } from "react";
-import { categories } from "../utils/utils.js"; // utils.js 파일에서 categories 객체 import
-
-export default class Tab extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      selectedCategory: "공부",
-      selectedSubcategory: categories["공부"][0],
-    };
-  }
-
-  //대분류 변경
-  handleCategoryChange = category => {
-    this.setState({
-      selectedCategory: category,
-      selectedSubcategory: categories[category][0],
-    });
+  const handleCategoryChange = category => {
+    setSelectedCategory(category);
+    setSelectedSubcategory(categories[category][0]);
   };
 
-  //소분류 변경
-  handleSubcategoryChange = subcategory => {
-    this.setState({ selectedSubcategory: subcategory });
+  const handleSubcategoryChange = subcategory => {
+    setSelectedSubcategory(subcategory);
   };
 
-  render() {
-    const { selectedCategory, selectedSubcategory } = this.state;
+  return (
+    <div className="flex flex-col tab-container bg-white rounded-md p-4 ">
+      <div className="flex flex-wrap gap-2 w-full">
+        {Object.keys(categories).map(category => (
+          <button
+            key={category}
+            onClick={() => handleCategoryChange(category)}
+            className={`flex-1 py-2 px-4 rounded-t-lg text-xl font-nps-bold ${
+              selectedCategory === category ? "bg-light-green text-black" : "bg-white text-gray"
+            }`}
+          >
+            {category}
+          </button>
+        ))}
+      </div>
 
-    return (
-      <div className="flex flex-col tab-container bg-white rounded-md p-4 ">
-        <div className="flex flex-wrap gap-2 w-full">
-          {Object.keys(categories).map(category => (
+      <div className="flex flex-wrap w-full bg-light-green justify-center">
+        <div className="flex flex-wrap gap-3 my-4 w-auto">
+          {categories[selectedCategory].map(subcategory => (
             <button
-              key={category}
-              onClick={() => this.handleCategoryChange(category)}
-              className={`flex-1 py-2 px-4 rounded-t-lg ${
-                selectedCategory === category ? "bg-light-green text-black" : "bg-white text-gray"
+              key={subcategory}
+              onClick={() => handleSubcategoryChange(subcategory)}
+              className={`w-44 h-12 py-2 px-4 rounded-2xl shadow-md ${
+                selectedSubcategory === subcategory
+                  ? "bg-dark-green text-white"
+                  : "bg-white text-black"
               }`}
             >
-              {category}
+              {subcategory}
             </button>
           ))}
         </div>
-        <div className="flex flex-wrap w-full bg-light-green justify-center">
-          <div className="flex flex-wrap gap-2 my-4 w-auto">
-            {categories[selectedCategory].map(subcategory => (
-              <button
-                key={subcategory}
-                onClick={() => this.handleSubcategoryChange(subcategory)}
-                className={`w-28 h-10 py-2 px-4 rounded-lg shadow-md ${
-                  selectedSubcategory === subcategory
-                    ? "bg-dark-green text-white"
-                    : "bg-white text-black"
-                }`}
-              >
-                {subcategory}
-              </button>
-            ))}
-          </div>
-        </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+export default Tab;
