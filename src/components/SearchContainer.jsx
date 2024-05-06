461d74495a12e3a7c849a6720389c479c947d23f
ramrami-B <hrb.harper@gmail.com>
2024-05-06 21:11:18 +0900

    Feat: 헤더에서 검색 컴포넌트 띄우고 닫기, 방만들기 모달 띄우기

--- src/components/SearchContainer.jsx
+++ src/components/SearchContainer.jsx
@@ -1,8 +1,19 @@
-const SearchContainer = () => {
+import PropTypes from "prop-types";
+
+const SearchContainer = ({ isOpen, setIsOpen }) => {
+  const handleClickCloseIcon = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div className="absolute top-0 left-0 w-screen h-screen bg-white">
       <div className="w-3/4 m-auto flex flex-col">
-        <span className="material-symbols-outlined self-end my-5 cursor-pointer">close</span>
+        <span
+          className="material-symbols-outlined self-end my-5 cursor-pointer"
+          onClick={handleClickCloseIcon}
+        >
+          close
+        </span>
         <div className="flex items-center border-[0.5px] border-gray rounded-md p-3">
           <span className="material-symbols-outlined">search</span>
           <input
@@ -13,6 +24,15 @@ const SearchContainer = () => {
       </div>
     </div>
   );
+};
+
+SearchContainer.propTypes = {
+  isOpen: PropTypes.bool,
+  setIsOpen: PropTypes.any,
+};
+
+SearchContainer.defaultProps = {
+  isOpen: true,
 };
 
 export default SearchContainer;
