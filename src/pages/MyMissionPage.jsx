c9e269d21d9579f19c724d0a9aadc296f140131e
ramrami-B <hrb.harper@gmail.com>
2024-05-14 17:07:10 +0900

    Feat: 나의미션방 페이지 생성

--- src/pages/MyMissionPage.jsx
+++ src/pages/MyMissionPage.jsx
@@ -1,7 +1,40 @@
+import React, { useEffect, useState } from "react";
+import SadMussuk from "../assets/sad_mussuk.svg";
+import { Button } from "../components/Button";
+import { useNavigate } from "react-router-dom";
+import { List } from "../components/List";
+import { missions } from "../utils/dummy-datas";
+
 const MyMissionPage = () => {
+  const [myMissionList, setMyMissionList] = useState([]);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setMyMissionList(missions);
+  }, []);
+
   return (
     <div className="page">
-      <h1 className="text-3xl font-bold underline">MyMissionPage</h1>
+      {myMissionList.length === 0 ? (
+        <div className="absolute top-[15%] left-[25%] bg-light-green rounded-3xl p-20 w-1/2 m-auto shadow-xl flex flex-col items-center gap-3">
+          <svg width="130px" viewBox="0 0 150 165">
+            <SadMussuk />
+          </svg>
+          <div className="text-center">
+            <p className="text-xs">나의 미션방이 비어있어요</p>
+            <p> 다른 미션방을 둘러보세요</p>
+          </div>
+          <Button label="모든 미션방 보러가기" onClick={() => navigate("/missions")}></Button>
+        </div>
+      ) : (
+        <div className="w-2/3 m-auto mt-10">
+          <p>🔥 오늘도 미션으로 불태워요!</p>
+          {myMissionList.map((mission, idx) => {
+            return <List key={idx} mission={mission} />;
+          })}
+        </div>
+      )}
     </div>
   );
 };
