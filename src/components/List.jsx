75823975b6a53cb09f1e34d86b0e75068ada0505
ramrami-B <hrb.harper@gmail.com>
2024-05-15 18:59:08 +0900

    Feat: 별 클릭 부분과 방 제목 부분 분리

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -4,13 +4,14 @@ import FullStar from "../assets/full_star.svg";
 import { useState } from "react";
 import Modal from "./modal/Modal";
 import JoinRoom from "./modal/JoinRoom";
+import { useLocation } from "react-router-dom";
 
 export const List = ({ mission }) => {
   const [isClickBookmarked, setIsClickBookmarked] = useState(mission.isBookmarked);
   const [isOpenDeleteModal, setIsOpenDeleteModal] = useState(false);
   const [isOpenMissionModal, setIsOpenMissionModal] = useState(false);
 
-  const page = window.location.pathname.split("/")[1];
+  const location = useLocation();
 
   const handlerClickStar = () => {
     setIsClickBookmarked(!isClickBookmarked);
@@ -25,36 +26,33 @@ export const List = ({ mission }) => {
   };
 
   return (
-    <div
-      className="flex justify-between items-center w-full h-[8vh] p-5 my-3 bg-white rounded-lg border-[0.5px] border-gray shadow hover:cursor-pointer hover:shadow-sm"
-      onClick={handlerClickList}
-    >
-      <div className="flex gap-5 items-center">
-        {page === "mymissions" ? (
-          <div onClick={handlerClickStar}>
-            <svg width="35" height="35" viewBox="0 0 40 40">
-              {isClickBookmarked ? <FullStar /> : <EmptyStar />}
-            </svg>
-          </div>
-        ) : (
-          <span className="m-[-0.5rem]"></span>
-        )}
+    <div className="flex justify-between items-center w-full h-[8vh] my-3 bg-white rounded-lg border-[0.5px] border-gray shadow hover:cursor-pointer hover:shadow-sm">
+      {location.pathname === "/mymissions" ? (
+        <div onClick={handlerClickStar} className="p-5">
+          <svg width="35" height="35" viewBox="0 0 40 40">
+            {isClickBookmarked ? <FullStar /> : <EmptyStar />}
+          </svg>
+        </div>
+      ) : (
+        <p>&ensp;&ensp;</p>
+      )}
+      <div className="flex items-center w-full h-full" onClick={handlerClickList}>
         <p className="nps-bold text-lg">{mission.title}</p>
       </div>
-      {page === "mymissions" ? (
+      {location.pathname === "/mymissions" ? (
         <span
-          className="material-symbols-outlined text-2xl hover:text-3xl"
+          className="material-symbols-outlined p-5 text-2xl hover:text-3xl"
           onClick={handlerClickTrash}
         >
           delete
         </span>
       ) : (
-        <div className="flex items-center gap-1">
+        <div className="flex items-center gap-1 p-5">
           <span className="material-symbols-outlined text-2xl">groups</span>
           <span className="text-lg">{mission.participantsCount}</span>
         </div>
       )}
-      {isOpenMissionModal && (window.location.pathname === "/mymissions") | mission.isMine ? (
+      {isOpenMissionModal && (location.pathname === "/mymissions" || mission.isMine) ? (
         <Modal
           title={mission.title}
           contents={mission.description}
